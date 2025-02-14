dc8d79b83cedf09f2a46da18428b7990e4e2c3b2
BUsuper <merzlyakov96@gmail.com>
2025-02-15 00:49:20 +1000

    Make TodoList access actual todos in state

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,13 @@
-import { useSelector } from 'react-redux'
 import { TodoForm, TodosList } from '../components/';
 import './App.css'
 
 function App() {
-  const todosIdsList = useSelector(state => state.ids);
 
   return (
     <>
       <TodoForm></TodoForm>
-      <TodosList todos={[{id: 1, task: "Cat", completed: false}, {id: 2, task: "Dog", completed: false}, {id: 3, task: "Fish", completed: true}]}/>
-      <TodosList completed={true} todos={[{id: 1, task: "Cat", completed: false}, {id: 2, task: "Dog", completed: true}, {id: 3, task: "Fish", completed: true}]}/>
+      <TodosList/>
+      <TodosList completed={true}/>
     </>
   )
 }
--- src/components/TodosList/TodosList.jsx
+++ src/components/TodosList/TodosList.jsx
@@ -1,15 +1,22 @@
 import { List, ListItem } from '@mui/material/';
+import { useSelector } from 'react-redux'
 
 // TODO: Add checkbox!
-export function TodosList ({ todos, completed = false }) {
+export function TodosList ({ completed = false }) {
+    const todosIdsList = useSelector(state => state.todos.ids);
+    const todosEntries = useSelector(state => state.todos.entities);
+
+    const todos = todosIdsList.map(item => todosEntries[item]).filter(item => item.completed === completed);
+
     return <>
         <List>
             {
                 // Filter out todos that are completed or not (depends of the completed parameter)
                 // Render the filtered list of todos
-                todos.filter(item => item.completed === completed).map((item, index) => {
+                todos.map(item => {
                     return <ListItem key={item.id}>{item.task}</ListItem>
                 })
+                
             }
         </List>
     </>
