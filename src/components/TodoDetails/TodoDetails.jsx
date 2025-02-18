cc20f5ec29d8e00e4b082cb1e3bcd60ed27caecc
BUsuper <merzlyakov96@gmail.com>
2025-02-18 20:25:44 +1000

    Add TodoDetails component

--- src/components/TodoDetails/TodoDetails.jsx
+++ src/components/TodoDetails/TodoDetails.jsx
@@ -1,8 +1,16 @@
 import { TextField } from "@mui/material";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectTodo } from "../../features/todosSlice";
 
+export function TodoDetails({ id }) {
+    const todo = useSelector(selectTodo(id))
 
-export function TodoDetails () {
-    return (
-        <TextField></TextField>
-    );
+    const [taskText, setTodoText] = useState(todo.task);
+    const [notesText, setNotesText] = useState(todo.notes);
+    // Do I really need the disabled attribute?
+    return (<>
+        <TextField slotProps={{readOnly: true}} disabled value={taskText} label="Todo"></TextField>
+        <TextField slotProps={{readOnly: true}} disabled value={notesText} label="Notes"></TextField>
+    </>);
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { TodosList } from './TodosList/TodosList'
-export { TodoForm } from './TodoForm/TodoForm'
-export { TodoDetails } from './TodoDetails'
+export { TodosList } from './TodosList/TodosList';
+export { TodoForm } from './TodoForm/TodoForm';
+export { TodoDetails } from './TodoDetails/TodoDetails';
