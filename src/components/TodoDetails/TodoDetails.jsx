75d817dacbca7c97101c793fe3d90202b4dd9372
BUsuper <merzlyakov96@gmail.com>
2025-02-19 21:15:37 +1000

    Add todo editing functionality to TodoDetails

--- src/components/TodoDetails/TodoDetails.jsx
+++ src/components/TodoDetails/TodoDetails.jsx
@@ -1,16 +1,64 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { useState } from "react";
-import { useSelector } from "react-redux";
-import { selectTodo } from "../../features/todosSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTodo, updateTodo } from "../../features/todosSlice";
 
 export function TodoDetails({ id }) {
     const todo = useSelector(selectTodo(id))
 
+    const [isEditingActive, setIsEditingActive] = useState(false);
     const [taskText, setTodoText] = useState(todo.task);
     const [notesText, setNotesText] = useState(todo.notes);
-    // Do I really need the disabled attribute?
-    return (<>
-        <TextField slotProps={{readOnly: true}} disabled value={taskText} id={`taskText${id}`} label="Todo"></TextField>
-        <TextField slotProps={{readOnly: true}} disabled value={notesText} id={`notesText${id}`} label="Notes"></TextField>
-    </>);
+
+    const dispatch = useDispatch();
+
+    // This is a copy of a function from TodoForm
+    // TODO: Import this function instead of copying it from TodoForm
+    const handleUserInput = (e, setter) => {
+        const userInput = e.target.value;
+        setter(userInput);
+    }
+
+    const handleTriggerEditing = () => {
+        setIsEditingActive(prev => !prev);
+    }
+
+    const handleSaving = (id) => {
+        const todo = {
+            id,
+            task: taskText,
+            notes: notesText,
+        }
+
+        dispatch(updateTodo(todo));
+    }
+
+    const handleCancelEditing = () => {
+        setTodoText(todo.task);
+        setNotesText(todo.notes);
+        setIsEditingActive(false);
+    }
+
+    // Yes, you need to stop the event from bubbling up to the TodoList
+    return (<div onClick={(e) => e.stopPropagation()}>
+        <TextField 
+            slotProps={{readOnly: true}}
+            disabled={!isEditingActive}
+            value={taskText}
+            onChange={(e) => handleUserInput(e, setTodoText)}
+            id={`taskText${id}`} 
+            label="Todo">
+        </TextField>
+        <TextField 
+            slotProps={{readOnly: true}} 
+            disabled={!isEditingActive} 
+            value={notesText}
+            onChange={(e) => handleUserInput(e, setNotesText)} 
+            id={`notesText${id}`} 
+            label="Notes">
+        </TextField>
+        <Button onClick={handleTriggerEditing}>Edit</Button>
+        <Button onClick={() => handleSaving(id)}>Save</Button>
+        <Button onClick={handleCancelEditing}>Cancel</Button>
+    </div>);
 }
