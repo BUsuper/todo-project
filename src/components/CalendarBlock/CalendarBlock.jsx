f67854fcea5e2bd5038b9faccb2ce577ead06e68
BUsuper <merzlyakov96@gmail.com>
2025-02-23 21:48:27 +1000

    Add calendar placeholder

--- src/components/CalendarBlock/CalendarBlock.jsx
+++ src/components/CalendarBlock/CalendarBlock.jsx
@@ -3,7 +3,7 @@ import { Grid2 } from '@mui/material';
 export function CalendarBlock() {
     return (
         <Grid2 size={{sm: 12, md: 4}}>
-            
+            Calendar goes here he he he
         </Grid2>
     );
 }
