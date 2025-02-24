6c1186c70db4347568d5c15f2e070db7c4d4f35a
BUsuper <merzlyakov96@gmail.com>
2025-02-24 22:21:32 +1000

    Add basic MUIX calendar to the CalendarBlock

--- src/components/CalendarBlock/CalendarBlock.jsx
+++ src/components/CalendarBlock/CalendarBlock.jsx
@@ -1,9 +1,13 @@
 import { Grid2 } from '@mui/material';
+import { DateCalendar, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 export function CalendarBlock() {
     return (
         <Grid2 size={{sm: 12, md: 4}}>
-            Calendar goes here he he he
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="de">
+                <DateCalendar></DateCalendar>
+            </LocalizationProvider>
         </Grid2>
     );
 }
