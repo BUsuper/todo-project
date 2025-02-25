48394f669747515578dd07338c9c4404f1bc7173
BUsuper <merzlyakov96@gmail.com>
2025-02-25 14:56:24 +1000

    Whap App contents into a Container

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,16 @@
-import { Grid2 } from '@mui/material';
+import { Container, Grid2 } from '@mui/material';
 import { CalendarBlock, TodoBlock } from '../components/';
 import './App.css'
 
 function App() {
 
   return (
-    <Grid2 container spacing={1}>
-      <CalendarBlock />
-      <TodoBlock />
-    </Grid2>
+    <Container>
+      <Grid2 container spacing={1}>
+        <CalendarBlock />
+        <TodoBlock />
+      </Grid2>
+    </Container>
   )
 }
 
