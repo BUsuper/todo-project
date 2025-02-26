667a7b68de7b146d17efb4567ce7244d5874b3de
BUsuper <merzlyakov96@gmail.com>
2025-02-26 15:38:27 +1000

    Make the Calendar save the selected date in state

--- src/components/CalendarBlock/CalendarBlock.jsx
+++ src/components/CalendarBlock/CalendarBlock.jsx
@@ -1,12 +1,20 @@
 import { Grid2 } from '@mui/material';
 import { DateCalendar, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useState } from 'react';
 
 export function CalendarBlock() {
+    const [selectedDate, setSelectedDate] = useState("");
+
+    const handleDateSelection = (date) => {
+        setSelectedDate(`${date.year()}-${date.month() + 1}-${date.date()}`);
+    }
+
     return (
         <Grid2 size={{sm: 12, md: 4}}>
             <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
-                <DateCalendar></DateCalendar>
+                <DateCalendar onChange={handleDateSelection}></DateCalendar>
+                Selected date: {selectedDate}
             </LocalizationProvider>
         </Grid2>
     );
