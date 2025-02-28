6b64ef26fe9fea3e9abb017b394e652d495179bc
BUsuper <merzlyakov96@gmail.com>
2025-02-28 19:46:05 +1000

    Make Badge color depend on whether the todo is overdue

--- src/components/CalendarDay/CalendarDay.jsx
+++ src/components/CalendarDay/CalendarDay.jsx
@@ -1,5 +1,6 @@
 import { Badge } from '@mui/material';
 import { PickersDay } from '@mui/x-date-pickers';
+import { getToday } from '../../utils';
 
 export function CalendarDay(props) {
     // daysWithTodos shouldn't be passed to PickerDays (React doesn't recognise it as a correct prop)
@@ -8,12 +9,14 @@ export function CalendarDay(props) {
     // Now I just need to compare the date of this day to a list of days with a todo
     const hasTodo = daysWithTodos.has(`${day.year()}-${day.month() + 1}-${day.date()}`);
     const isHighlighted = !outsideCurrentMonth && hasTodo;
+    const today = getToday();
+    const overdue = `${day.year()}-${day.month() + 1}-${day.date()}` < today;
 
     return (
         <Badge 
             variant={isHighlighted && "dot"}
             overlap="circular"
-            color='primary'
+            color={overdue ? "error" : "primary"}
         >
             <PickersDay
                 day={day}
