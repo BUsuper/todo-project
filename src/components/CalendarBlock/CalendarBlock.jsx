8bc9a6e9f0437515336c14f46e1cbef546a6e038
BUsuper <merzlyakov96@gmail.com>
2025-03-05 15:36:12 +1000

    Fix a small bug when CalendarBlock wouldn't be centered when displayed on a narrow screen

--- src/components/CalendarBlock/CalendarBlock.jsx
+++ src/components/CalendarBlock/CalendarBlock.jsx
@@ -5,7 +5,7 @@ import { TodosCalendar, FiltersDetails } from '../../components';
 export function CalendarBlock() {
 
     return (
-        <Grid2 size={{sm: 12, md: 5}}>
+        <Grid2 size={{sm: 12, md: 5}} sx={{margin:"0 auto"}}>
             <TodosCalendar />
             <FiltersDetails />
         </Grid2>
