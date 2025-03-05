ae1fe96e9b7011594b12b59b9fa93275fcf93c4b
BUsuper <merzlyakov96@gmail.com>
2025-03-05 15:42:12 +1000

    Remove a redundant Container component (App.css provides padding already)

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,14 @@
-import { Container, Grid2 } from '@mui/material';
+import { Grid2 } from '@mui/material';
 import { CalendarBlock, TodoBlock } from '../components/';
 import './App.css'
 
 function App() {
 
   return (
-    <Container>
-      <Grid2 container spacing={1}>
-        <CalendarBlock />
-        <TodoBlock />
-      </Grid2>
-    </Container>
+    <Grid2 container spacing={1}>
+      <CalendarBlock />
+      <TodoBlock />
+    </Grid2>
   )
 }
 
